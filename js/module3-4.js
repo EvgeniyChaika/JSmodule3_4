a2b3973ccfe5fda6b809bed5af0eb1d8e48588ac
Evgeniy Chaika <evgeniy.chaika.83@gmail.com>
2016-03-22 20:49:58 +0200

    First commit

---
+++ js/module3-4.js
@@ -0,0 +1,63 @@
+var table = {
+    title: "Тест по программированию",
+    lines: ['Вопрос №1', 'Вопрос №2', 'Вопрос №3'],
+    answers: [
+        ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+        ['Вариант ответа №4', 'Вариант ответа №5', 'Вариант ответа №6'],
+        ['Вариант ответа №7', 'Вариант ответа №8', 'Вариант ответа №9']
+    ],
+    createLine: function () {
+        var wrapper = document.createElement('div');
+        wrapper.classList.add('wrapper');
+        var body = document.querySelector('body');
+        body.appendChild(wrapper);
+
+        var title = document.createElement('h3');
+        title.classList.add('title');
+        title.innerHTML = this.title;
+        wrapper.appendChild(title);
+
+        var list = document.createElement('ol');
+        list.classList.add('list');
+        wrapper.appendChild(list);
+
+        for (var k = 0; k < this.lines.length; k++) {
+            var listLine = document.createElement('li');
+            listLine.innerHTML = this.lines[k];
+            list.appendChild(listLine);
+            var listUl = document.createElement('ul');
+            listLine.appendChild(listUl);
+
+            for (var m = 0; m < this.answers[k].length; m++) {
+                var innerListLine = document.createElement('li');
+                innerListLine.classList.add('list-line');
+
+                var lineCheckbox = document.createElement('input');
+                lineCheckbox.classList.add('checkbox');
+                lineCheckbox.setAttribute("type", "checkbox");
+
+                var labelCheckbox = document.createElement('label');
+                labelCheckbox.classList.add('label');
+
+
+                var spanCheckbox = document.createElement('span');
+                spanCheckbox.classList.add('span');
+                spanCheckbox.innerHTML = this.answers[k][m];
+
+                innerListLine.appendChild(labelCheckbox);
+                labelCheckbox.appendChild(lineCheckbox);
+                labelCheckbox.appendChild(spanCheckbox);
+
+                listUl.appendChild(innerListLine);
+            }
+        }
+        var submit = document.createElement('input');
+        submit.setAttribute("type", "submit");
+        submit.setAttribute("value", "Проверить мои результаты");
+        submit.classList.add('submit');
+        wrapper.appendChild(submit);
+
+    }
+};
+
+table.createLine();
